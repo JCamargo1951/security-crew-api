32611a3a9174f33af96ba28d2dda7a8e8ed485cf
jcamargo1951 <camargo.cp11@gmail.com>
2026-01-25 19:56:57 -0600

    Creando factorie de links

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Link>
@@ -17,7 +19,21 @@ class LinkFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(3),
+            'url' => $this->faker->url(),
+            'shortener_url' => $this->faker->slug(),
+            'visibility' => 'public',
+            'expires_at' => $this->faker->optional(0.5)->dateTimeBetween('+1 day', '+30 days'),
+            'user_id' => User::factory(),
+            'password' => null,
         ];
+    }
+
+    public function protectedLink(): static
+    {
+        return $this->state(fn() => [
+            'visibility' => 'private',
+            'password' => Hash::make('secret')
+        ]);
     }
 }
