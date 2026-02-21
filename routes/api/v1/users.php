f93fb4841ff6eff1afc9b0dc23bb12b6a7152c8f
jcamargo1951 <camargo.cp11@gmail.com>
2026-02-20 22:13:10 -0600

    update: check_user auth status endpoint

--- routes/api/v1/users.php
+++ routes/api/v1/users.php
@@ -3,7 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
    return new UserResource($request->user());
-});
+})->name('user');
+
+Route::get('/check_user', function () {
+   $user = Auth::user();
+   if (!$user) {
+      return response([
+         'data' => [
+            'auth' => false,
+            'user' => null,
+         ]
+      ]);
+   }
+   return response([
+      'data' => [
+         'auth' => true,
+         'user' => new UserResource($user),
+      ]
+   ]);
+})->name('user.check_user');
